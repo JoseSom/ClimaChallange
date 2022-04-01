295158106e24302d1b824a8c2dcc9911b8739811
Jose Luis Somilleda <joseluissom004@gmail.com>
2022-03-31 23:45:58 -0600

    Se añade settings y se ajusta sharedPreferences

--- app/src/main/java/com/example/climachallange/settingsModule/model/SettingsInteractor.kt
+++ app/src/main/java/com/example/climachallange/settingsModule/model/SettingsInteractor.kt
@@ -1,4 +1,15 @@
 package com.example.climachallange.settingsModule.model
 
+import android.content.Context
+import android.util.Log
+import androidx.preference.ListPreference
+import com.example.climachallange.common.utils.TAG
+import com.example.climachallange.common.utils.getJsonDataFromAsset
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+import org.json.JSONException
+import org.json.JSONObject
+
 class SettingsInteractor {
 }
